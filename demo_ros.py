dc526db8ec606677db8d955a7a43a33a0933244d
Jiang, Rong <835293711@qq.com>
2023-11-13 16:05:17 +0800

    通过ros读取l515图片

---
+++ demo_ros.py
@@ -0,0 +1,160 @@
+""" Demo to show prediction results.
+    Author: chenxi-wang
+"""
+
+import os
+import sys
+import numpy as np
+import open3d as o3d
+import argparse
+import importlib
+import scipy.io as scio
+from PIL import Image
+
+import torch
+from graspnetAPI import GraspGroup
+
+import rospy
+import sensor_msgs
+from cv_bridge import CvBridge
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(ROOT_DIR, 'models'))
+sys.path.append(os.path.join(ROOT_DIR, 'dataset'))
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+
+from graspnet import GraspNet, pred_decode
+from graspnet_dataset import GraspNetDataset
+from collision_detector import ModelFreeCollisionDetector
+from data_utils import CameraInfo, create_point_cloud_from_depth_image
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--checkpoint_path', required=True, help='Model checkpoint path')
+parser.add_argument('--num_point', type=int, default=20000, help='Point Number [default: 20000]')
+parser.add_argument('--num_view', type=int, default=300, help='View Number [default: 300]')
+parser.add_argument('--collision_thresh', type=float, default=0.01, help='Collision Threshold in collision detection [default: 0.01]')
+parser.add_argument('--voxel_size', type=float, default=0.01, help='Voxel Size to process point clouds before collision detection [default: 0.01]')
+cfgs = parser.parse_args()
+
+COLOR_IMAGE=None
+DEPTH_IMAGE=None
+
+def get_net():
+    # Init the model
+    net = GraspNet(input_feature_dim=0, num_view=cfgs.num_view, num_angle=12, num_depth=4,
+            cylinder_radius=0.05, hmin=-0.02, hmax_list=[0.01,0.02,0.03,0.04], is_training=False)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net.to(device)
+    # Load checkpoint
+    checkpoint = torch.load(cfgs.checkpoint_path)
+    net.load_state_dict(checkpoint['model_state_dict'])
+    start_epoch = checkpoint['epoch']
+    print("-> loaded checkpoint %s (epoch: %d)"%(cfgs.checkpoint_path, start_epoch))
+    # set model to eval mode
+    net.eval()
+    return net
+
+def get_and_process_data(data_dir):
+    # load data
+    color = COLOR_IMAGE # 1280 x 720 x 3 , 0.0 ~ 1.0
+    depth = DEPTH_IMAGE # 1280 x 720, 0 ~ 1590
+    workspace_mask = np.array(Image.open(os.path.join(data_dir, 'workspace_mask.png'))) # 1280 x 720, True or False
+    meta = scio.loadmat(os.path.join(data_dir, 'meta.mat'))
+    intrinsic = meta['intrinsic_matrix'] # 3 x 3
+    factor_depth = meta['factor_depth'] # 1000.0
+
+    # generate cloud
+    camera = CameraInfo(1280.0, 720.0, intrinsic[0][0], intrinsic[1][1], intrinsic[0][2], intrinsic[1][2], factor_depth)
+    cloud = create_point_cloud_from_depth_image(depth, camera, organized=True)
+
+    # get valid points
+    mask = (workspace_mask & (depth > 0))
+    cloud_masked = cloud[mask]
+    color_masked = color[mask]
+
+    # sample points
+    if len(cloud_masked) >= cfgs.num_point:
+        idxs = np.random.choice(len(cloud_masked), cfgs.num_point, replace=False)
+    else:
+        idxs1 = np.arange(len(cloud_masked))
+        idxs2 = np.random.choice(len(cloud_masked), cfgs.num_point-len(cloud_masked), replace=True)
+        idxs = np.concatenate([idxs1, idxs2], axis=0)
+    cloud_sampled = cloud_masked[idxs]
+    color_sampled = color_masked[idxs]
+
+    # convert data
+    cloud = o3d.geometry.PointCloud()
+    cloud.points = o3d.utility.Vector3dVector(cloud_masked.astype(np.float32))
+    cloud.colors = o3d.utility.Vector3dVector(color_masked.astype(np.float32))
+    end_points = dict()
+    cloud_sampled = torch.from_numpy(cloud_sampled[np.newaxis].astype(np.float32))
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    cloud_sampled = cloud_sampled.to(device)
+    end_points['point_clouds'] = cloud_sampled
+    end_points['cloud_colors'] = color_sampled
+
+    return end_points, cloud
+
+def get_grasps(net, end_points):
+    # Forward pass
+    with torch.no_grad():
+        end_points = net(end_points)
+        grasp_preds = pred_decode(end_points)
+    gg_array = grasp_preds[0].detach().cpu().numpy()
+    gg = GraspGroup(gg_array)
+    return gg
+
+def collision_detection(gg, cloud):
+    mfcdetector = ModelFreeCollisionDetector(cloud, voxel_size=cfgs.voxel_size)
+    collision_mask = mfcdetector.detect(gg, approach_dist=0.05, collision_thresh=cfgs.collision_thresh)
+    gg = gg[~collision_mask]
+    return gg
+
+def vis_grasps(gg, cloud):
+    gg.nms()
+    gg.sort_by_score()
+    gg = gg[:50]
+    grippers = gg.to_open3d_geometry_list()
+    camera_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
+    o3d.visualization.draw_geometries([cloud, *grippers, camera_frame])
+
+def demo(event):
+    if COLOR_IMAGE is None or DEPTH_IMAGE is None:
+        print('Images are not ready')
+        return
+    net = get_net()
+    end_points, cloud = get_and_process_data('doc/example_data')
+    gg = get_grasps(net, end_points)
+    if cfgs.collision_thresh > 0:
+        gg = collision_detection(gg, np.array(cloud.points))
+    vis_grasps(gg, cloud)
+
+def color_image_callback(msg):
+    # 创建CvBridge对象
+    bridge = CvBridge()
+    # 将ROS图像消息转换为OpenCV图像格式
+    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    # 将OpenCV图像转换为NumPy数组
+    global COLOR_IMAGE
+    COLOR_IMAGE = np.array(cv_image) / 255.0 # rgb的通道顺序, 0.0-1.0, float64
+    # 在这里对NumPy数组进行处理
+    return
+
+def depth_image_callback(msg):
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    global DEPTH_IMAGE
+    DEPTH_IMAGE = np.array(cv_image)
+    return
+    
+
+if __name__ == '__main__':
+    rospy.init_node('graspnet_ros_demo')
+
+    # 创建图像订阅者
+    _ = rospy.Subscriber('/camera/color/image_raw', sensor_msgs.msg.Image, color_image_callback)
+    _ = rospy.Subscriber('/camera/aligned_depth_to_color/image_raw', sensor_msgs.msg.Image, depth_image_callback)
+    _ = rospy.Timer(rospy.Duration(0.5), demo)
+
+    # 进入ROS循环
+    rospy.spin()
